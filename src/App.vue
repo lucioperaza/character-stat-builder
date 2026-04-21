f5015d5bf4e9f14b955cab52453d4a105ed46cef
lperaza2 <lperaza2@stu.southtexascollege.edu>
2026-04-21 18:47:11 -0500

    initialized character stats project with tailwind, prettier, eslint and made a simple template for the in the app.vue

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+    <h1 class="text-3xl font-bold">Character Stats Builder</h1>
+  </div>
+</template>
